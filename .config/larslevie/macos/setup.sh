8884c9f148b6138f2030551201486724884ed8b7
Lars Levie <larslevie@gmail.com>
2020-06-11 06:00:15 -0500

    Don’t hardcode computer name

--- .config/larslevie/macos/setup.sh
+++ .config/larslevie/macos/setup.sh
@@ -10,12 +10,10 @@ sudo -v
 # # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Set computer name
-# while [[ ! $computername ]]; do
-#   read -p "What is this computer's name? " computername
-# done
-
-computername="Svolvær"
+Set computer name
+while [[ ! $computername ]]; do
+  read -p "What is this computer's name? " computername
+done
 
 sudo scutil --set ComputerName $computername
 sudo scutil --set LocalHostName `echo $computername | iconv -t ASCII//TRANSLIT`
