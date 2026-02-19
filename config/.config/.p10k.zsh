469d55843eadbdba81176ae74b78e3ec15933ae1
larslevie <larslevie@gmail.com>
2026-02-18 19:42:47 -0600

    style: switch terminal theme to bearded-monokai-reversed
    Drop Dracula theme from ghostty. Swap k9s skin to
    bearded-monokai-reversed with full color scheme definition. Adjust p10k
    directory foreground colors to complement the new palette.

--- config/.config/.p10k.zsh
+++ config/.config/.p10k.zsh
@@ -214,14 +214,14 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=31
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=75
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=103
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=110
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=39
